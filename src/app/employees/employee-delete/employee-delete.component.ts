dcb96d7ec603f2c771eaf668d148ddd4742c7ad5
muhdarifrawi <muhdarifrawi@gmail.com>
2023-01-29 12:43:55 +0800

    added the delete route.

--- src/app/employees/employee-delete/employee-delete.component.ts
+++ src/app/employees/employee-delete/employee-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeeService } from 'src/app/Services/EmployeeServices/employee.service';
 
 @Component({
   selector: 'app-employee-delete',
@@ -6,11 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-delete.component.css']
 })
 export class EmployeeDeleteComponent implements OnInit{
-  constructor(){}
+  userId:string = ""
+
+  constructor(
+    private activatedRoute:ActivatedRoute,
+    private empService:EmployeeService,
+    private snackBar:MatSnackBar,
+    private router:Router
+    ){}
+
   ngOnInit(): void {
-      
+    this.activatedRoute.params.subscribe(data=>{
+      this.userId=data["id"]
+    })
+
+    // if(this.userId !== ""){
+    //   this.load = true
+    // }
+    // else{
+    //   this.router.navigate(['../employees/all']);
+    // }
   }
+
   deleteEmployeeAction(){
-    console.log("Employee deleted")
+    console.log("Deleting employee ...")
+    this.empService.deleteEmployee(this.userId).subscribe(data => {
+          console.log("employee deleted")
+          this.snackBar.open("User deleted successfully!", "Dismiss")
+          
+    })
+    this.router.navigate(['../employees/all']);
   }
 }
